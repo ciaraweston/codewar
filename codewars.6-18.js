cc9f331d1d71610617af3668cc08a2c5c06ef2a1
ciaraweston <cweston0727@outlook.com>
2021-09-06 20:41:42 -0400

    codewar.6-18

---
+++ codewars.6-18.js
@@ -0,0 +1,13 @@
+function step(g, m, n) {
+  for (let i = m; i <= n; i++) {
+    if (isPrime(i) && isPrime(i + g)) { return [i, i + g] }
+  }
+  return null;
+}
+function isPrime(n) {
+  let rt = Math.sqrt(n);
+  for (let i = 2; i <= rt; i++) {
+    if (n % i === 0) return false;
+  }
+  return n !== 1;
+}
