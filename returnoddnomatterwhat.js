fa910b4966b87188ebe16c2b220e7210b54e2340
ciaraweston <cweston0727@outlook.com>
2022-01-27 01:33:56 -0500

    returnoddnomatterwhat.js

---
+++ returnoddnomatterwhat.js
@@ -0,0 +1,7 @@
+// Given the integer n return odd numbers as they are, but subtract 1 from even numbers.
+
+//   Note:
+
+// Your solution should be 22 or less characters long.
+
+alwaysOdd = n => n % 2 ? n : n - 1
